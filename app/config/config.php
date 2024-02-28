16dfd046a86879a8c9075435f1816d624be35071
akashvaidya23 <akash.vaidya23@gmail.com>
2024-02-28 07:38:46 +0530

    Controller Changes.

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+    // App Route
+    define("APPROOT",dirname(dirname(__FILE__)));
+    define("URLROOT",'http://localhost/my_mvc');
+
+    // SITE NAME
+    define("SITENAME","MY_MVC");
+?>
--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -1,15 +1,19 @@
 <?php
-    class PageController{
+    class PageController extends Controller{
         public function __construct(){
             echo "Inside PageController <br>";
         }
 
         public function index(){
-            echo "This is index from pageController";
+            echo "This is index from pageController <br>";
+            $data = ['title' => "Welcome"];
+            $this->view('pages/index',$data);
         }
 
         public function about($id){
-            echo "this is about " . $id;
+            $data = ["title" => "About Page"];
+            echo "this is about " . $id . "<br>";
+            $this->view("pages/about",$data);
         }
 
         public function create(){
